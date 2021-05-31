f6c178d154c72f2a64e07c996aae14935600cd62
chazalsio2 <chazal.hassani@gmail.com>
2021-06-01 01:35:59 +0400

    7éme commit

--- css/skins/script.js
+++ css/skins/script.js
@@ -9,16 +9,33 @@ btnClose.addEventListener('click',closeModal);
 }
 function closeModal() {
     overlay.style.display='none';
-=======
-var btnPopup = document.getElementById('btnPopup');
-var overlay = document.getElementById('overlay');
-btnPopup.addEventListener('click',openMoadl);
-function openMoadl() {
-overlay.style.display='block';
-var btnClose = document.getElementById('btnClose');
-btnClose.addEventListener('click',closeModal);
 }
-function closeModal() {
-    overlay.style.display='none';
->>>>>>> d2a0064f1d40c115fdff89b5b66ed0fd33055a82
-    }
+Splitting();
+ScrollOut({
+   targets: '[data-splitting]'
+});
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
